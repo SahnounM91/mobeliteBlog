481593fe933b0f510c457a2e259b3a736e94c97e
sahnoun mabrouk <sahnoun.mabrouk@gmail.com>
2018-09-26 22:08:57 +0100

    Extra point : Add Scroll to load in home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,21 +8,49 @@ import { PostsProvider } from '../../providers/posts/posts';
 })
 export class HomePage {
 
-  posts: any;
+  tmpPosts: any = [];
+  items = [];
+  page = 1;
+  perPage = 0;
+  totalData = 0;
+  totalPage = 0;
+  posts: any = [];
 
   constructor(public navCtrl: NavController, public postsProvider: PostsProvider) {
     this.getPosts();
   }
 
+  doInfinite(infiniteScroll) {
+    setTimeout (() => {
+      this.postsProvider.getPosts().subscribe(data => {
+        this.tmpPosts = data
+        let result = this.tmpPosts.slice(this.page *5);
+        for (let i = 1; i <= this.perPage; i ++) {
+            this.posts.push(result[i]);
+        }
+        console.log(this.posts)
+      })
+      this.page += 1;
+      infiniteScroll.complete ();
+    }, 500);
+  }
+
   getPosts() {
     this.postsProvider.getPosts()
     .subscribe(data => {
-      this.posts = data;
+      this.tmpPosts = data;
+      for (let i = 0; i <= 4; i ++) {
+        this.posts.push(this.tmpPosts[i]);
+      }
+      this.perPage = 5;
+      this.totalData = this.tmpPosts.length;
+      this.totalPage = 5;
     });
   }
+
+
   postDetails(id){
     this.navCtrl.push("PostPage", {id: id})
   }
-
 
 }
