7dd7a4288c9930f46b9f53f59f655f41611364f8
sahnoun mabrouk <sahnoun.mabrouk@gmail.com>
2018-09-27 10:41:30 +0100

    MITT-4 Check if network connection is work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Network } from '@ionic-native/network';
+import { AlertController } from 'ionic-angular';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -10,13 +12,33 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private alertCtrl: AlertController, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private network: Network) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    this.checkNetworkConnection()
+  }
+
+  checkNetworkConnection(){
+    // watch network for a disconnect
+    this.network.onDisconnect().subscribe(() => {
+      this.presentAlert("Check your network connection to load data");
+    });
+
+  }
+
+  // show alert when there is no connection
+  presentAlert(message) {
+    let alert = this.alertCtrl.create({
+      title: 'Network status',
+      subTitle: message,
+      buttons: ['ok']
+    });
+    alert.present();
   }
 }
 
